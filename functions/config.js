1af14d4d742cf4d7e3d6d1f259974bd266555664
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-08-02 06:10:46 +0630

    Update config.js

--- functions/config.js
+++ functions/config.js
@@ -5,7 +5,4 @@
 
 export const TELEGRAM_BOT_TOKEN_ENV = 'BOT_TOKEN';
 export const D1_BINDING_NAME = 'DB';
-export const ADMIN_IDS = [
-    123456789,
-    987654321
-];
+export const ADMIN_IDS = [8027567256,1273841502];

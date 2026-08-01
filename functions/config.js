e2c30a553bb607769d628d5c7d18de7ba335d40b
4 0 4 \ 2.0 [🇲🇲] <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-08-02 05:48:19 +0630

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,7 @@
+///////////////////////////////////////////////
+// Copyright (C) t.me/nkka404
+// Channel: https://t.me/premium_channel_404
+///////////////////////////////////////////////
+
+export const TELEGRAM_BOT_TOKEN_ENV = 'BOT_TOKEN';
+export const D1_BINDING_NAME = 'DB';
